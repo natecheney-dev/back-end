3899345f663f85d6e9c381ce673abefbdefb3fa9
natecheney-dev <natecheney@gmail.com>
2021-12-22 14:31:35 -0600

    working?

--- knexfile.js
+++ knexfile.js
@@ -38,24 +38,3 @@ module.exports = {
     pool: { min: 2, max: 10 },
   },
 }
-
-
-
-// const sharedConfig = {
-//     client: 'sqlite3',
-//     useNullAsDefault: true,
-//     migrations: { directory: './data/migrations' },
-//     pool: { afterCreate: (conn, done) => conn.run('PRAGMA foreign_keys = ON', done) },
-//   }
-  
-//   module.exports = {
-//     development: {
-//       ...sharedConfig,
-//       connection: { filename: './data/auth.db3' },
-//       seeds: { directory: './data/seeds' },
-//     },
-//     testing: {
-//       ...sharedConfig,
-//       connection: { filename: './data/test.db3' },
-//     },
-//   };
