dcc5e7938c06dc754873088bbd2493e75c967975
natecheney-dev <natecheney@gmail.com>
2021-12-22 13:15:49 -0600

    another one

--- api/server.js
+++ api/server.js
@@ -3,22 +3,25 @@ const helmet = require('helmet')
 const cors = require('cors')
 const server = express()
 
-
+const usersAuthRouter = require('./users/auth-router')
+const eventRouter = require('./events/events-router')
 const guestRouter = require('./guests/guests-router')
-// const eventRouter = require('./events/events-router')
-// const usersAuthRouter = require('./users/auth-router')
+
 
 server.use(express.json())
+server.use(helmet())
+server.use(cors())
 
-// server.use('/api/auth', usersAuthRouter)
-// server.use('/api/events', eventRouter)
+server.use('/api/auth', usersAuthRouter)
+server.use('/api/events', eventRouter)
 server.use('/api/guests', guestRouter)
 
+
 server.use((err, req, res, next) => {
-    res.status(err.status || 500).json({
-      message: err.message,
-      stack: err.stack
-    })
+  res.status(err.status || 500).json({
+    message: err.message,
+    stack: err.stack
   })
-  
-  module.exports = server
+})
+
+module.exports = server
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const server = require ('./api/server')
+require('dotenv').config()
 
-const PORT = 8080
+const server = require('./api/server')
+
+const PORT = process.env.PORT || 5000
 
 server.listen(PORT, () => {
-    console.log(`listening on port ${PORT}`)
+  console.log(`listening on ${PORT}`)
 })
